17457efc726c4772237b0dfd0f575d5014bbe49c
mjontop <mjontop0602@gmail.com>
2019-09-18 19:09:38 +0530

    selling option added

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,20 @@ $(document).ready(() => {
     $("#menu").click(() => {
         $(".tohide").slideToggle("slow")
     })
+    getmenuback()
 })
 
 let getmenuback = () => {
     if ($(window).width() > 768) {
-		$(".tohide").slideDown("fast")
-	}
-	else{
-		$(".tohide").slideUp("fast")
-	}
+        $(".tohide").slideDown("fast")
+    } else {
+        $(".tohide").slideUp("fast")
+    }
+    if ($(window).width() > 1080) {
+        $("#desc").slideDown("fast")
+    } else {
+        $("#desc").slideUp("fast")
+    }
 }
 
 $(window).on("resize", getmenuback)
